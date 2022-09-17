2994e7e9b590095fb6c5c2de5a863b9dfc9311a6
Marcel B. Daryanto <marcel.budiono@gmail.com>
2022-09-17 22:10:50 +0700

    using decimal for price

--- inventory/models.py
+++ inventory/models.py
@@ -13,7 +13,7 @@ class ItemCategory(models.Model):
 class Item(models.Model):
     barcode = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=100)
-    unit_price = models.FloatField()
+    unit_price = models.DecimalField(max_digits=20, decimal_places=2)
     category = models.ForeignKey(ItemCategory, on_delete=models.RESTRICT)
     notes = models.TextField(blank=True)
 
